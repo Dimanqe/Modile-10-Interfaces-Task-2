cca4c699257ca224aa4205dc424109cc65982bd0
Dimanqe <d.sankeev@gmail.com>
2023-09-07 13:33:40 +0300

    Dependancy injection implemented

--- Modile 10 Interfaces Task 2/Program.cs
+++ Modile 10 Interfaces Task 2/Program.cs
@@ -9,10 +9,13 @@ namespace Modile_10_Interfaces_Task_2
 {
     internal class Program
     {
+        static ILogger Logger { get; set; }
         static void Main(string[] args)
+
         {
             NumberFormatInfo numberFormatInfo = new NumberFormatInfo() { NumberDecimalSeparator = "." };
-            ICalculator calculator = new Calculator();
+            Logger = new logger();
+            var calculator = new Calculator(Logger);
 
             try
             {
@@ -26,17 +29,21 @@ namespace Modile_10_Interfaces_Task_2
 
                 double result = calculator.Add(num1, num2);
                 Console.WriteLine($"Сумма чисел: {result}");
+
             }
             catch (FormatException ex)
             {
+                calculator.Error(ConsoleColor.Red);
                 Console.WriteLine($"Ошибка формата: {ex.Message}");
             }
             catch (Exception ex)
             {
+                calculator.Error(ConsoleColor.Red);
                 Console.WriteLine($"Произошла ошибка: {ex.Message}");
             }
             finally
             {
+                calculator.Error(ConsoleColor.Green);
                 Console.WriteLine("Программа завершена.");
             }
         }
@@ -53,16 +60,50 @@ namespace Modile_10_Interfaces_Task_2
             }
         }
 
-        public interface ICalculator
+        public interface ILogger
         {
+            void Event(string text);
+            void Error(ConsoleColor color);
+        }
+
+        public class logger : ILogger
+        {
+            public void Error(ConsoleColor color)
+            {
+                Console.BackgroundColor = color;
+            }
+
+            public void Event(string text)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine(text);
+            }
+        }
+
+        public interface ICalculator
+
+        {
+            void Error(ConsoleColor color);
+
             double Add(double a, double b);
         }
 
         public class Calculator : ICalculator
         {
+            ILogger Logger { get; }
+            public Calculator(ILogger logger)
+            {
+                Logger = logger;
+            }
+
             public double Add(double a, double b)
             {
+                Logger.Event("Операция сложения выполнена");
                 return a + b;
+            }
+            public void Error(ConsoleColor color)
+            {
+                Console.BackgroundColor = color;
             }
         }
     }
